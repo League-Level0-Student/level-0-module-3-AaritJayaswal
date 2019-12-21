fa8b1bcb37bead6490e1404a0b9ff086ed0cc815
League Student <league@iMac-5.attlocal.net>
2019-12-21 12:56:09 -0800

    worked on lvl 0 module 3

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -11,21 +11,45 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "December 14th";
+		String dadsBirthday = "February 1st";
+		String myBirthday = "July 15th";
+		String sistersBirthday= "April 25th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String bday = JOptionPane.showInputDialog(null,"Who's birthday do you want to know?");
 		// 3. Print out what the user typed
-		
+		System.out.println(bday);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+if (bday.contentEquals("mom")) {
+	JOptionPane.showMessageDialog(null,momsBirthday);
+	 System.exit(0);
+}
+		//print mom's birthday
 		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+else if (bday.contentEquals("dad")) {
+	JOptionPane.showMessageDialog(null,dadsBirthday);
+	 System.exit(0);
+}
 
+// print dad's birthday
+		// 6. if user asked for your name
+		
+else if (bday.contentEquals("Aarit")) {
+	JOptionPane.showMessageDialog(null,myBirthday);
+	 System.exit(0);
+}
+
+else if (bday.contentEquals("Shaina")) {
+	JOptionPane.showMessageDialog(null,sistersBirthday);
+	 System.exit(0);
+}
+
+// print myBirthday
+		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+else {
+	
+	JOptionPane.showMessageDialog(null,"I don't remember that person's birthday");
+}
 	} 
 }
