71b91c02b633e7a8457b15a5aeb4b5481beaabe7
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-01-11 11:00:10 -0800

    Update Obedient_robot.java

--- src/_16_obedient_robot/Obedient_robot.java
+++ src/_16_obedient_robot/Obedient_robot.java
@@ -1,5 +1,29 @@
 package _16_obedient_robot;
 
-public class Obedient_robot {
+import org.jointheleague.graphical.robot.Robot;
 
+public class Obedient_robot {
+	
+	public static void main(String[]args) {
+		
+		
+		drawSquare();
+		
+		
+	
+	}
+
+	
+	public static void drawSquare() {
+		Robot joe = new Robot();
+		joe.setSpeed(100);
+		joe.penDown();
+		for(int  i = 0; i<4;i++) { 
+		joe.move(100);
+		joe.turn(90);
+		}
+		
+	}
+	
+	
 }
